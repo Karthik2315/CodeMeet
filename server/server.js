cb091c3511c5941148df418f019218b618f4d0ab
Karthik P <padiyil2004@gmail.com>
2025-11-05 09:46:59 +0530

    downloaded clerk

--- server/server.js
+++ server/server.js
@@ -1,1 +1,11 @@
 import express from 'express';
+import { ENV } from './lib/env.js';
+import connectDB from './lib/mongoDB.js';
+
+const app = express();
+const PORT = ENV.PORT || 5000;
+
+connectDB();
+app.listen(PORT,(req,res) => {
+  console.log(`Server is running on ${PORT}`);
+})
