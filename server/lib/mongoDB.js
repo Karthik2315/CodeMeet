0284dd55566989d6662e59b0bba5219d7ed4eb6d
Karthik P <padiyil2004@gmail.com>
2025-12-17 11:14:55 +0530

    added "server" in scripts in package.json

--- server/lib/mongoDB.js
+++ server/lib/mongoDB.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
 
 const connectDB = async() => {
   mongoose.connection.on('connected',() => {
     console.log("Database Connected");
   });
-
-  await mongoose.connect(`${process.env.MONGODB_URI}/CodeMeet`)
+  await mongoose.connect(`${ENV.MONGODB_URL}/CodeMeet`)
 };
 
-export default connectDB;
+export default connectDB; 
