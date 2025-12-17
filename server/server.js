588af26c773578f52333137f9d96ecb3ca5b1cde
Karthik P <padiyil2004@gmail.com>
2025-12-17 14:29:53 +0530

    downloaded dependencies and added the details from env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import { ENV } from './lib/env.js';
 import connectDB from './lib/mongoDB.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = ENV.PORT || 5000;
-
+app.use(cors({
+  origin:ENV.CLIENT_URL,
+  credentials:true
+}));
+app.use(express.json());
 await connectDB();
 app.listen(PORT,() => {
   console.log(`Server is running on ${PORT}`);
