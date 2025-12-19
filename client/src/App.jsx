eb9acbf76fb6bba9540fedfe529941427a987462
Karthik P <padiyil2004@gmail.com>
2025-12-19 10:49:00 +0530

    some changes to check whether inngest and clerk is working together

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 const App = () => {
   return (
-    <div>
-      
+    <div style={{ padding: "40px" }}>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button>Sign In</button>
+        </SignInButton>
+      </SignedOut>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
