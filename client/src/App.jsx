cfda1eeae958d22ac4a2c28fe9f6f42ccfa9e139
Karthik P <padiyil2004@gmail.com>
2026-02-28 16:25:11 +0530

    setted basic stuffs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,22 @@ import {
   SignInButton,
   UserButton,
 } from "@clerk/clerk-react";
+import {Navigate, Route,Routes} from 'react-router-dom'
+import HomePage from "./Pages/HomePage";
+import ProblemsPage from "./Pages/ProblemsPage";
+import { useAuth } from "@clerk/clerk-react";
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
+  const {isSignedIn} = useAuth();
   return (
-    <div style={{ padding: "40px" }}>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <button>Sign In</button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      
-    </div>
+    <>
+    <Toaster position="top-center" toastOptions={{duration:3000}}/>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>} />
+      </Routes> 
+    </>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,10 @@ import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { BrowserRouter } from "react-router-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -12,13 +16,17 @@ if (!PUBLISHABLE_KEY) {
   throw new Error('Missing Clerk Publishable Key')
 }
 
+const queryClient = new QueryClient();
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ClerkProvider> 
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ClerkProvider> 
+    </QueryClientProvider>
   </BrowserRouter> 
 )
