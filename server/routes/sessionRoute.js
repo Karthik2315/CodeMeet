3c3707ef510f34cb8eb616f19ef5e873347a9bc6
Karthik P <padiyil2004@gmail.com>
2026-02-28 13:32:00 +0530

    added join and end session routes

--- server/routes/sessionRoute.js
+++ server/routes/sessionRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { createSession, getRecentSessions, getSessionById } from '../controllers/sessionController.js';
+import { createSession, endSession, getRecentSessions, getSessionById, joinSession } from '../controllers/sessionController.js';
 import { get } from 'mongoose';
 
 const sessionRoute = express.Router();
@@ -9,6 +9,8 @@ sessionRoute.post("/",protectRoute,createSession);
 sessionRoute.get('/active',protectRoute,getActiveSession);
 sessionRoute.get('/recent',protectRoute,getRecentSessions);
 sessionRoute.get('/:id',protectRoute,getSessionById);
+sessionRoute.get('/:id/join',protectRoute,joinSession);
+sessionRoute.get('/:id/end',protectRoute,endSession);
 
 
 export default sessionRoute;
