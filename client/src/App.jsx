5139cf0415a7283702b6bf3d97d998556d5760aa
Karthik P <padiyil2004@gmail.com>
2026-03-01 13:26:47 +0530

    added route for "/dashboard"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,14 +10,17 @@ import HomePage from "./Pages/HomePage";
 import ProblemsPage from "./Pages/ProblemsPage";
 import { useAuth } from "@clerk/clerk-react";
 import { Toaster } from 'react-hot-toast'
+import DashBoardPage from "./Pages/DashBoardPage";
 
 const App = () => {
-  const {isSignedIn} = useAuth();
+  const {isSignedIn,isLoaded} = useAuth();
+  if(!isLoaded) return null;
   return (
     <>
     <Toaster position="top-center" toastOptions={{duration:3000}}/>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashBoardPage /> : <Navigate to={"/"}/>}/>
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"}/>} />
       </Routes> 
     </>
